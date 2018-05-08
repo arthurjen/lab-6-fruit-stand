46645a3bf440c5a3ada8b65ad55f8f5e0754ada6
Arthur Jen <arthurljen@gmail.com>
2018-05-08 16:37:29 -0700

    quantity buttons bugged

--- app.js
+++ app.js
@@ -1,36 +1,88 @@
 /* globals beerList */
+
 'use strict';
-function addName(i) {
+
+addBeer();
+
+function addBeer() {
+    for(let i = 0; i < beerList.length; i++) {
+        createDivs(i);
+        addName(i);
+        addImage(i);
+        addDetails(i);
+        addQuantityButton(i);
+    }
+}
+
+function createDivs(i) {
     const parent = document.getElementById('beer-list');
-    const child = document.createElement('li');
+    const div = document.createElement('div');
+    div.setAttribute('id', 'beer-' + i);
+    div.setAttribute('class', 'beer');
+    parent.appendChild(div);
+}
+
+function addName(i) {
+    const parent = document.getElementById('beer-' + i);
+    const child = document.createElement('h2');
     child.textContent = beerList[i].name;
     parent.appendChild(child);
 }
 
 function addImage(i) {
-    const parent = document.getElementById('beer-list');
+    const parent = document.getElementById('beer-' + i);
     const image = document.createElement('img');
     image.src = './images/' + beerList[i].image;
     parent.appendChild(image);
 }
 
 function addDetails(i) {
-    const parent = document.getElementById('beer-list');
+    const parent = document.getElementById('beer-' + i);
     let child = document.createElement('p');
     child.textContent = beerList[i].brewery;
     parent.appendChild(child);
 
-    child = document.createElement('p')
+    child = document.createElement('p');
     child.textContent = beerList[i].abv;
+    parent.appendChild(child);
+
+    
+}
+
+function addQuantityButton(i) {
+    const parent = document.getElementById('beer-' + i);
+    let child = document.createElement('button');
+    child.setAttribute('onclick', 'decreaseQuantity()');
+    // child.setAttribute('id', 'beer-btn-' + i);
+    child.textContent = '-';
+    parent.appendChild(child);
+
+    child = document.createElement('span');
+    child.setAttribute('id', i);
+    child.textContent = beerList[i].quantity;
+    parent.appendChild(child);
+
+    child = document.createElement('button');
+    child.setAttribute('onclick', 'increaseQuantity()');
+    // child.setAttribute('id', 'beer-btn-' + i);
+    child.textContent = '+';
     parent.appendChild(child);
 }
 
-function addBeer() {
-    for(let i = 0; i < beerList.length; i++) {
-        addName(i);
-        addImage(i);
-        addDetails(i);
-    }
+
+
+function increaseQuantity() {
+    let quantity = document.querySelector('span');
+    const index = quantity.getAttribute('id');
+    beerList[index].increaseQuantity();
+    console.log(beerList[index].quantity);
+    quantity.textContent = beerList[index].quantity;
 }
 
-addBeer();
+function decreaseQuantity() {
+    let quantity = document.querySelector('span');
+    let index = quantity.getAttribute('id');
+    beerList[index].decreaseQuantity();
+    console.log(beerList[index].quantity);
+    quantity.textContent = beerList[index].quantity;
+}
--- beer.js
+++ beer.js
@@ -1,32 +1,47 @@
 /* exported beerList */
 
 'use strict';
-function Beer(name, image, brewery, abv) {
+
+function Beer(name, image, brewery, abv, color, quantity) {
     this.name = name;
     this.image = image;
     this.brewery = brewery;
     this.abv = abv;
+    this.color = color;
+    this.quantity = quantity;
+    this.increaseQuantity = function() {
+        this.quantity++;
+    };
+    this.decreaseQuantity = function() {
+        this.quantity--;
+    };
 }
 
 const wanderlust = new Beer(
     'Wanderlust',
     'wanderlust.jpg',
     'Breakside',
-    '6.2%'
+    '6.2%',
+    'brown',
+    '18'
 );
 
 const lunchBreak = new Beer(
     'Lunch Break',
     'lunchBreak.jpg',
     'Breakside',
-    '4.7%'
+    '4.7%',
+    'blue',
+    '36'
 );
 
 const breakside = new Beer(
     'Breakside',
     'breakside.jpg',
     'Breakside',
-    '6.2%'
+    '6.2%',
+    'white',
+    '0'
 );
 
 const beerList = [
